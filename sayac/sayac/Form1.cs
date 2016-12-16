95ae0af536eba814f06d74dafaccd51c998d7ab5
ozgurdogusaymaz <ozzgurdogusaymaz@hotmail.com>
2016-12-16 23:57:20 +0300

    sayac

---
+++ sayac/sayac/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sayac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayi = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayi = sayi + 1;
+            label1.Text = sayi.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sayi = sayi + 3;
+            label2.Text = sayi.ToString();
+        }
+    }
+}
